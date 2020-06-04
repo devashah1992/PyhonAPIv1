af256c17424dc8b89ad10504510e92e09fc2a365
devashah1992 <shahdev20792@gmail.com>
2020-06-04 18:44:56 +0530

    Created API using Python and Flask V1

---
+++ user.py
@@ -0,0 +1,79 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+
+
+class User():
+    TABLE_NAME = 'users'
+
+    def __init__(self, _id, username, password):
+        self.id = _id
+        self.username = username
+        self.password = password
+
+
+#initially was self --> changed to cls ans no @classmethod
+#cls so we do not need to use "User" everywhere .. just cls will do
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM {table} WHERE username=?".format(table=cls.TABLE_NAME)
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()  #fetch only first row
+        if row: #if user is none
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+    @classmethod
+    def find_by_id(cls, _id):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM {table} WHERE id=?".format(table=cls.TABLE_NAME)
+        result = cursor.execute(query, (_id,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+
+class UserRegister(Resource):
+    TABLE_NAME = 'users'
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if User.find_by_username(data['username']):  #if not None
+            return {"message": "User with that username already exists."}, 400
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO {table} VALUES (NULL, ?, ?)".format(table=self.TABLE_NAME)
+        cursor.execute(query, (data['username'], data['password']))
+
+        connection.commit()
+        connection.close()
+
+        return {"message": "User created successfully."}, 201
